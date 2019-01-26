906d4783f2af19755509379596adba4bbbc66a63
zhuangzhiheng <zhuangzhiheng@71aeon.com>
2019-01-26 09:48:34 +0800

    2019/1/26

--- Test.py
+++ Test.py
@@ -1,14 +1,7 @@
 import asyncio
 import aiomysql
+l="united states of america"
 
-async def db_setup(app, loop):
-    pool = await aiomysql.create_pool(**app.config['MYSQL'],loop=loop)
-    print("mysql start successfully")       #初始化aiomysql
-    return pool
 
-async def setup_db(app, loop):
-    app.db = await db_setup(app, loop)
-
-if __name__ == '__main__':
-    loop=asyncio.get_event_loop()
-    loop.run_until_complete(setup_db())
+s={1,2,3,4}
+print(3 in s)
