dfd5623aacc6a9ab2bfe10b0fd36e20ebf8162a9
zhuangzhiheng <zhuangzhiheng@71aeon.com>
2019-01-28 15:59:18 +0800

    2019/1/28

--- Test.py
+++ Test.py
@@ -1,7 +1,20 @@
 import asyncio
-import aiomysql
-l="united states of america"
+import aiohttp
 
 
-s={1,2,3,4}
-print(3 in s)
+async def fetch():
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.post("https://www.instagram.com/explore/locations/?__a=1", data={'page': 1}, ssl=False) as resp:
+                if resp.status in [200, 201]:
+                    data = await resp.json()
+                    print(data)
+                else:
+                    print(resp.status)
+    except Exception as e:
+        print("!!!!Exception:", e)
+
+
+if __name__ == '__main__':
+    loop=asyncio.get_event_loop()                               #定义事件循环
+    loop.run_until_complete(fetch())  #类似多线程里面的join(),运行完指定的协程,关闭loop
